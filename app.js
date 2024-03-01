88630df2bdb66b1cc603705c8db5615185530c3e
kshitijasaswade <61146709+kshitijasaswade123@users.noreply.github.com>
2024-03-01 14:51:55 +0530

    cors  install in app

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const mongoose = require("mongoose")
-require('dotenv/config');
-const userRoute = require("./route/userRoute")
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
+const userRoute = require("./route/userRoute");
+const cors = require("cors");
 
-const app =express();
+const app = express();
 app.use(express.json());
-app.get('/',(req,res)=>{
-    res.send("HOME")
-})
-app.use('/api/userdata',userRoute);
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("HOME");
+});
+app.use("/api/userdata", userRoute);
 
 app.listen(process.env.PORT);
 
-async function dbconnection(){
-    try {
-        const res = await mongoose.connect(process.env.CONNECT);
-        const data = await res.default;
-        console.log(data.STATES.connected);
-    } catch (error) {
-        console.log(error.message);
-    }
+async function dbconnection() {
+  try {
+    const res = await mongoose.connect(process.env.CONNECT);
+    const data = await res.default;
+    console.log(data.STATES.connected);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 dbconnection();
-
